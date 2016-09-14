2283768036477d3d290c7349e60df3a51c08ccca
sunamt <sune@liquidmedia.dk>
2016-09-15 00:55:33 +0200

    sune

--- Assets/Scripts/Utils/LoadTimer.cs
+++ Assets/Scripts/Utils/LoadTimer.cs
@@ -1,14 +1,23 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using VRStandardAssets.Utils;
 
 public class LoadTimer : MonoBehaviour {
 
+	[SerializeField] private VRCameraFade m_CameraFade; 
+
 	public string m_MenuSceneName = "insertScene";
 	public float Timer = 15f;
+	public float FadeTimer = 4f;
+
 		IEnumerator Start () {
 		yield return new WaitForSeconds (Timer);
 		//Application.LoadLevel(SceneToLoad);
+
+		m_CameraFade.FadeOut(true);
+		yield return new WaitForSeconds (FadeTimer);
 		SceneManager.LoadScene(m_MenuSceneName, LoadSceneMode.Single);
 		}
 	}
